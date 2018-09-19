503f530b11f9075a6112fb1dc53c788fcb0f7c7e
xbl <blxie@toughtworks.com>
2018-09-19 17:54:42 +0800

    refactor(server):

--- src/server.js
+++ src/server.js
@@ -10,42 +10,46 @@ app.use((req, res, next) => {
   next();
 });
 
+const handler = (req, res, config, webApi) => {
+  if (webApi.controller) {
+    const controller = webApi.controller.split('#');
+    // eslint-disable-next-line
+    const moduleCtrl = require(`${config.controller}/${controller[0]}`);
+    const fn = moduleCtrl[controller[1]];
+    if (typeof fn === 'function') fn.call(app, req, res, webApi);
+    return;
+  }
+
+  const response = webApi.responses[0];
+  if (!response) {
+    res.status(404).send('no set reponse or example');
+    return;
+  }
+
+  if (isRedirectCode(response.code)) {
+    res.redirect(response.location);
+    return;
+  }
+
+  if (response.mimeType) res.type(response.mimeType);
+  res.status(response.code);
+  let { body } = response;
+  if (body) {
+    if (Array.isArray(config.plugins)) {
+      config.plugins.forEach((plugin) => {
+        // eslint-disable-next-line
+        body = require(plugin)(body);
+      });
+    }
+    res.send(body);
+  }
+};
+
 exports.setConfig = (config) => {
   const webApiArr = readRaml(config);
   webApiArr.forEach((webApi) => {
     app[webApi.method](webApi.absoluteUri, (req, res) => {
-      if (webApi.controller) {
-        const controller = webApi.controller.split('#');
-        // eslint-disable-next-line
-        const moduleCtrl = require(`${config.controller}/${controller[0]}`);
-        const fn = moduleCtrl[controller[1]];
-        if (typeof fn === 'function') fn.call(app, req, res, webApi);
-        return;
-      }
-
-      const response = webApi.responses[0];
-      if (!response) {
-        res.status(404).send('no set reponse or example');
-        return;
-      }
-
-      if (isRedirectCode(response.code)) {
-        res.redirect(response.location);
-        return;
-      }
-
-      if (response.mimeType) res.type(response.mimeType);
-      res.status(response.code);
-      let { body } = response;
-      if (body) {
-        if (Array.isArray(config.plugins)) {
-          config.plugins.forEach((plugin) => {
-            // eslint-disable-next-line
-            body = require(plugin)(body);
-          });
-        }
-        res.send(body);
-      }
+      handler(req, res, config, webApi);
     });
   });
 };
