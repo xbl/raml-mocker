fbf0a5f8f5a8179bd1079afef6d9414a39c8b100
xbl <blxie@toughtworks.com>
2018-11-07 15:11:40 +0800

    test(*): add raml example file

--- src/read_raml.js
+++ src/read_raml.js
@@ -2,26 +2,50 @@ const { join } = require('path');
 const raml = require('raml-1-parser');
 const { isRedirectCode } = require('./util');
 
-module.exports = (config) => {
-  const apiJSON = raml.loadApiSync(join(config.raml, config.main), { serializeMetadata: false });
+const getTypeMap = apiJSON => {
+  const typeMap = {};
+  const typeDeclarationArr = apiJSON.types();
+  typeDeclarationArr.forEach(type => {
+    const typeName = type.name();
+    const { properties } = type.toJSON({ serializeMetadata: false })[typeName];
 
+    const requiredArr = [];
+    Object.keys(properties).forEach(key => {
+      const property = properties[key];
+      // const propertyType = property.type[0];
+      // console.log(propertyType);
+      const { items, required, name } = property;
+      if (required) {
+        requiredArr.push(name);
+      }
+      if (items) {
+        console.log(items);
+      }
+      // console.log(property);
+    });
+    typeMap[typeName] = properties;
+  });
+  return typeMap;
+};
+
+const getWebApiArr = apiJSON => {
   const webApiArr = [];
-  apiJSON.allResources().forEach((resource) => {
+  apiJSON.allResources().forEach(resource => {
     const absoluteUri = resource.absoluteUri();
-    resource.methods().forEach((method) => {
+    resource.methods().forEach(method => {
       const webApi = { absoluteUri, method: method.method() };
-      method.annotations().forEach((annotation) => {
+      method.annotations().forEach(annotation => {
         const json = annotation.toJSON();
         if (json.name !== 'controller') return;
         webApi.controller = json.structuredValue;
       });
 
       webApi.responses = [];
-      method.responses().forEach((response) => {
+      method.responses().forEach(response => {
         const code = response.code().value();
         // 30x
         if (isRedirectCode(code)) {
-          response.headers().forEach((typeDeclaration) => {
+          response.headers().forEach(typeDeclaration => {
             if (typeDeclaration.name().toLowerCase() === 'location') {
               const redirectURL = typeDeclaration.type()[0];
               webApi.responses.push({ code, location: redirectURL });
@@ -29,9 +53,15 @@ module.exports = (config) => {
           });
           return;
         }
-        response.body().forEach((typeDeclaration) => {
+        response.body().forEach(typeDeclaration => {
           const mimeType = typeDeclaration.name();
           const example = typeDeclaration.example();
+          // const type = typeDeclaration.type()[0];
+          // if (type.includes('[]')) {
+          //   // console.log(type);
+          // } else {
+          //   console.log(typeMap[type]);
+          // }
           if (example) {
             webApi.responses.push({ code, body: example.value(), mimeType });
           }
@@ -40,6 +70,14 @@ module.exports = (config) => {
       webApiArr.push(webApi);
     });
   });
+};
 
-  return webApiArr;
+module.exports = config => {
+  const apiJSON = raml.loadApiSync(join(config.raml, config.main), {
+    serializeMetadata: false
+  });
+  // const typeMap =
+  getTypeMap(apiJSON);
+  // console.log(typeMap);
+  return getWebApiArr(apiJSON);
 };
