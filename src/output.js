187a93dbc954bf98faa7603324394fe7ac185e59
xbl <blxie@toughtworks.com>
2019-01-25 11:20:57 +0800

    refactor(output): 精简代码，更安全的输出

--- src/output.js
+++ src/output.js
@@ -3,11 +3,10 @@ const chalk = require('chalk');
 function Output(host, maxCount) {
   this.host = host;
   this.maxCount = maxCount;
-  this.logArr = [];
-  this.valid = true;
   this.successCount = 0;
   this.failCount = 0;
 
+  console.log(`HOST: ${this.host}`);
   Output.prototype.push = (valid, message, validInfo, request, beginTime) => {
     let color = 'green';
     let icon = '✔';
@@ -15,12 +14,11 @@ function Output(host, maxCount) {
     if (!valid) {
       color = 'red';
       icon = '✖';
-      this.valid = false;
       this.failCount++;
     } else {
       this.successCount++;
     }
-    this.logArr.push(
+    console.log(
       chalk`{${color} ${icon} 请求：[${request.method}]} {underline ${
         request.path
       }} {gray ${Date.now() -
@@ -30,14 +28,12 @@ function Output(host, maxCount) {
   };
 
   Output.prototype.print = () => {
-    if (this.logArr.length < this.maxCount) return;
-    console.log(`HOST: ${this.host}`);
-    this.logArr.forEach(_ => console.log(_));
+    if (this.successCount + this.failCount < this.maxCount) return;
     console.log(chalk`{green ${this.successCount} tests passed}`);
     if (this.failCount > 0) {
       console.log(chalk`{red ${this.failCount} tests failed} `);
+      process.exit(1);
     }
-    process.exit(this.valid ? 0 : 1);
   };
 }
 
