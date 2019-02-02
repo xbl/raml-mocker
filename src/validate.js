6c52fc31d552fd4d13b4788dd999f1608547af20
xbl <blxie@toughtworks.com>
2019-02-02 09:20:54 +0800

    refactor(validate): validate Schema in one file

--- src/validate.js
+++ src/validate.js
@@ -1,6 +1,26 @@
 const Ajv = require('ajv');
+const { jsonPath } = require('../src/util');
 
-exports.validate = (definitionSchema, schema, data) => {
+const processMessage = (valid, error, data) => {
+  let validInfo = '';
+  let msg = '';
+  if (!valid) {
+    let result = data;
+    const { message, dataPath } = error;
+    msg = message;
+    if (dataPath) {
+      result = jsonPath(data, dataPath);
+    }
+    validInfo = `info:${dataPath}\n${JSON.stringify(result, null, '\t')}\n`;
+  }
+  return {
+    valid,
+    message: msg,
+    validInfo
+  };
+};
+
+const validateSchema = (definitionSchema, schema, data) => {
   const ajv = new Ajv();
   let validate;
   try {
@@ -9,11 +29,12 @@ exports.validate = (definitionSchema, schema, data) => {
     if (error.missingRef) {
       throw Error(`Missing custom type "${error.missingRef.split('/').pop()}"`);
     }
+    throw error;
   }
   const valid = validate(data);
+  const error = !valid && validate.errors.pop();
 
-  return {
-    valid,
-    error: !valid && validate.errors.pop()
-  };
+  return processMessage(valid, error, data);
 };
+
+exports.validateSchema = validateSchema;
