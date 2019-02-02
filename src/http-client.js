54510c2dd1a6f4d5f424865595bbb969ebcd8d5d
xbl <blxie@toughtworks.com>
2019-02-02 09:25:08 +0800

    refactor(raml-runner): http client and send with data

---
+++ src/http-client.js
@@ -0,0 +1,37 @@
+const axios = require('axios');
+const { replaceUriParameters } = require('./util');
+
+let baseURL;
+exports.setHost = host => {
+  axios.defaults.baseURL = host;
+  baseURL = host;
+};
+
+const send = async (webApi, uriParameters, queryParameter = {}, body = {}) => {
+  if (!baseURL) throw Error('Please set HOST!');
+  let requestPath = webApi.absoluteUri;
+  if (uriParameters) {
+    replaceUriParameters(requestPath, (match, expression) => {
+      requestPath = requestPath.replace(match, uriParameters[expression]);
+    });
+  }
+
+  const response = await axios(requestPath, {
+    method: webApi.method,
+    data: body,
+    params: queryParameter
+  });
+
+  const { runner } = webApi;
+  if (runner) {
+    const { after } = runner;
+    // eslint-disable-next-line
+    const afterModule = require(resolve(after));
+    if (typeof afterModule === 'function') {
+      afterModule(axios, response);
+    }
+  }
+  return response;
+};
+
+exports.send = send;
