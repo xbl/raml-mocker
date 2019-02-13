187ac16008022ad0aa1806806dfe2286217fc2d6
xbl <blxie@toughtworks.com>
2019-02-13 17:59:50 +0800

    feat(*): read har

--- src/har-convert/index.ts
+++ src/har-convert/index.ts
@@ -1,10 +1,44 @@
-const xhrFilter = require('./xhr');
+import xhrFilter from './xhr';
+import urlUtil from 'url';
+import RestAPI from '../models/rest-api';
 
-export const read = har => {
+const filterEmpty = obj => JSON.parse(JSON.stringify(obj));
+
+const toParameter = (queryStrings) =>
+  queryStrings.reduce((accumulator, {name, value}) => {
+    return Object.assign(accumulator, {[name]: value})
+  }, {});
+
+export const read = (har): RestAPI[] => {
   const json = JSON.parse(har);
-  return xhrFilter(json);
+  const entries = xhrFilter(json.log.entries);
+
+  return entries.map(entry => {
+    const { request, response } = entry;
+    const { url, method, queryString, postData } = request;
+    const newUrl = urlUtil.parse(url);
+
+    const {
+      status,
+      content: { mimeType, text }
+    } = response;
+
+    return filterEmpty({
+      url: newUrl.pathname,
+      description: `${method.toLowerCase()}${ newUrl.pathname.replace(/\//g, '_') }`,
+      method,
+      queryParameter: toParameter(queryString),
+      body: postData,
+      responses: [
+        {
+          code: status,
+          body: { mimeType, text }
+        }
+      ]
+    });
+  });
 };
 
-export const save = () => {
+export const save = (restAPIArr: RestAPI[]) => {
 
 };
--- test/har-convert.spec.ts
+++ test/har-convert.spec.ts
@@ -1,49 +1,49 @@
 import fs from 'fs';
 import test from 'ava';
 import { read } from '../src/har-convert';
+import RestAPI from '../src/models/rest-api';
 
 test.cb('Given a har file, then get xhr request arr', t => {
   fs.readFile(`${__dirname}/localhost.har`, 'utf8', (err, data) => {
     if (err) throw err;
-    const xhrArr = [
+    const restAPIArr: RestAPI[] = [
       {
         url: '/api/test/raml/orders/T012019011828586',
+        description: 'get_api_test_raml_orders_T012019011828586',
         method: 'GET',
-        queryString: [
+        queryParameter: {
+          param1: 'value1',
+        },
+        responses: [
           {
-            name: 'param1',
-            value: 'value1',
-            comment: ''
+            code: 200,
+            body: {
+              mimeType: 'application/json',
+              text: '{"name":"你好"}'
+            }
           }
-        ],
-        response: {
-          status: 200,
-          content: {
-            mimeType: 'application/json',
-            text: '{"name":"你好"}'
-          }
-        }
-      },
-      {
+        ]
+      }, {
         url: '/api/test/raml/orders/T012019011828586/redeem',
+        description: 'post_api_test_raml_orders_T012019011828586_redeem',
         method: 'POST',
-        queryString: [],
+        queryParameter: {},
         body: {
           mimeType: 'application/json;charset=UTF-8',
           text: '{"a":1,"b":2}'
         },
-        response: {
-          status: 200,
-          content: {
+        responses: [{
+          code: 200,
+          body: {
             mimeType: 'application/json',
             text: '{}'
           }
-        }
+        }]
       }
     ];
 
     const result = read(data);
-    t.deepEqual(result, xhrArr);
+    t.deepEqual(result, restAPIArr);
     t.end();
   });
 });
