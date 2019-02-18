a3e8e7134ee35ef27ae72f559c2a0314613ca175
xbl <blxie@toughtworks.com>
2019-02-18 15:28:09 +0800

    feat(to-raml): remove duplicate rest api

--- src/har-convert/to-raml.ts
+++ src/har-convert/to-raml.ts
@@ -3,10 +3,17 @@ import { template } from 'lodash';
 import RestAPI from '../models/rest-api';
 import { indentString, readFileAsync } from '../util';
 
+const filter = (restApiArr) => {
+  const map = {};
+  return restApiArr.filter(restAPI => {
+    return map[`${restAPI.method}_${restAPI.url} `] ? false : (map[`${restAPI.method}_${restAPI.url} `] = true);
+  })
+};
+
 const toRaml = async (restAPIArr: RestAPI[]) => {
   const str = await readFileAsync(`${__dirname}/template/api.ejs`, 'utf-8');
   const compiled = template(str, { 'imports' : { 'indentString': indentString }});
-  return compiled({ restAPIArr });
+  return compiled({ restAPIArr: filter(restAPIArr) });
 };
 
 export default toRaml;
--- test/to-raml.spec.ts
+++ test/to-raml.spec.ts
@@ -64,3 +64,82 @@ test('Given restAPI array, then get raml str', async t => {
   const result = await toRaml(restAPIArr);
   t.is(result.trim(), expectResult);
 });
+
+test('Given restAPI duplicate array, then get raml str', async t => {
+  const restAPIArr: RestAPI[] = [
+    {
+      url: '/api/test/raml/orders/T012019011828586',
+      description: 'get_api_test_raml_orders_T012019011828586',
+      method: 'GET',
+      queryParameter: {
+        param1: 'value1'
+      },
+      responses: [
+        {
+          code: 200,
+          body: {
+            mimeType: 'application/json',
+            text: '{"name":"你好"}'
+          }
+        }
+      ]
+    },{
+      url: '/api/test/raml/orders/T012019011828586',
+      description: 'get_api_test_raml_orders_T012019011828586',
+      method: 'GET',
+      queryParameter: {
+        param1: 'value2'
+      },
+      responses: [
+        {
+          code: 200,
+          body: {
+            mimeType: 'application/json',
+            text: '{"name":"你好"}'
+          }
+        }
+      ]
+    }, {
+      url: '/api/test/raml/orders/T012019011828586/redeem',
+      description: 'post_api_test_raml_orders_T012019011828586_redeem',
+      method: 'POST',
+      queryParameter: {},
+      body: {
+        mimeType: 'application/json;charset=UTF-8',
+        text: '{"a":1,"b":2}'
+      },
+      responses: [{
+        code: 200,
+        body: {
+          mimeType: 'application/json',
+          text: '{}'
+        }
+      }]
+    }
+  ];
+
+  const expectResult = `
+/api/test/raml/orders/T012019011828586:
+  get:
+    description: get_api_test_raml_orders_T012019011828586
+    queryParameters:
+      param1:
+        example: value1
+    responses:
+      200:
+        body:
+          example: |
+            {"name":"你好"}
+
+/api/test/raml/orders/T012019011828586/redeem:
+  post:
+    description: post_api_test_raml_orders_T012019011828586_redeem
+    responses:
+      200:
+        body:
+          example: |
+            {}
+`.trim();
+  const result = await toRaml(restAPIArr);
+  t.is(result.trim(), expectResult);
+});
