e6748f28c930549fb99dc47d61b26450d38d7828
xbl <blxie@toughtworks.com>
2019-04-01 13:41:41 +0800

    test(to-spec): unit test

--- src/har-convert/to-spec.ts
+++ src/har-convert/to-spec.ts
@@ -7,7 +7,7 @@ import Parameter from '@/models/parameter';
 const toSpec = async (restAPIArr: RestAPI[]) => {
   const str = await fs.readFile(`${__dirname}/template/test.spec.ejs`, 'utf-8');
   const compiled = template(str, { imports : { isJSONType, Parameter }});
-  return compiled({ restAPIArr});
+  return compiled({ restAPIArr}).trim();
 };
 
 export default toSpec;
--- test/har-convert/har-convert.spec.ts
+++ test/har-convert/har-convert.spec.ts
@@ -2,8 +2,9 @@ import test from 'ava';
 import path from 'path';
 import fs from '@/util/fs';
 import sinon from 'sinon';
-import { read, mergeRestApiToSpec } from '@/har-convert';
 import RestAPI from '@/models/rest-api';
+import { read, mergeRestApiToSpec, save } from '@/har-convert';
+import { tsInterfaces } from 'raml-definition-system';
 
 test('Given a har file, then get xhr request arr', async (t) => {
   const restAPIs: RestAPI[] = [
@@ -128,5 +129,67 @@ it('Case Name', async () => {
 
   const result = await mergeRestApiToSpec(restAPIs);
   t.is(expectResult, result.trim());
+  sinon.restore();
+});
+
+test.serial('Given restAPIs, When save Then appendFile spec str', async (t) => {
+  const restAPIs: RestAPI[] = [
+    {
+      url: '/api/test/raml/orders/T012019011828586',
+      description: 'get_api_test_raml_orders_T012019011828586',
+      method: 'GET',
+      queryParameters: [{
+        name: 'param1',
+        example: 'value2',
+      }],
+      responses: [
+        {
+          code: 200,
+          body: {
+            mimeType: 'application/json',
+            text: '{"name":"你好"}',
+          },
+        },
+      ],
+    },
+  ];
+
+  const expectResult = `
+const assert = require('assert');
+const { loadApi } = require('@xbl/raml-mocker');
+
+it('Case Name', async () => {
+  const getFn0 = loadApi('get_api_test_raml_orders_T012019011828586');
+  const { status: status0, data: data0 } = await getFn0({},{"param1":"value2"},{});
+
+  assert.equal(status0, 200);
+  // TODO: assert
+
+});
+`.trim();
+
+  const template = await fs.readFile(path.resolve(__dirname, '../../src/har-convert/template/test.spec.ejs'));
+  const callback = sinon.stub();
+  callback.onCall(0).resolves(`
+    {
+      "controller": "./controller",
+      "raml": "./test/har-convert",
+      "main": "api.raml",
+      "port": 3000,
+      "runner": {
+        "test": "http://localhost:3000"
+      }
+    }
+  `);
+  callback.onCall(1).resolves(template);
+  sinon.replace(fs, 'readFile', callback);
+
+  const target = '1.js';
+  sinon.replace(fs, 'appendFile', (fileTarget, str) => {
+    t.is(fileTarget, target);
+    t.is(str, expectResult);
+  });
+
+  await save(restAPIs, target);
   sinon.restore();
 });
