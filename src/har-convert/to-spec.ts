889e50d1b7dfd9127c521a8866429b41180c5405
xbl <x_bl@sina.com>
2020-08-27 09:35:36 +0800

    refactor(to-spec): extract constant

--- src/har-convert/to-spec.ts
+++ src/har-convert/to-spec.ts
@@ -4,8 +4,10 @@ import fs from '@/util/fs';
 import RestAPI from '@/models/rest-api';
 import Parameter from '@/models/parameter';
 
+const SPEC_TEMPLATE = `${__dirname}/template/test.spec.ejs`;
+
 const toSpec = async (restAPIArr: RestAPI[]) => {
-  const str = await fs.readFile(`${__dirname}/template/test.spec.ejs`, 'utf-8');
+  const str = await fs.readFile(SPEC_TEMPLATE, 'utf-8');
   const compiled = template(str, { imports : { isJSONType, Parameter }});
   return compiled({ restAPIArr}).trim();
 };
